4be23ee47b2823acc8ed487490668cb7c7ae081c
Victor Oliveira <victor.moura.amaral.oliveira@gmail.com>
2026-05-04 21:53:49 -0300

    feat(exercise-tag): finish exercise tag component

--- components/exercise-tags/exercise-tag.tsx
+++ components/exercise-tags/exercise-tag.tsx
@@ -3,14 +3,34 @@ import { Text, TouchableOpacity } from 'react-native';
 
 interface TagProps {
   label: string;
+  height?: number;
+  borderRadius?: number;
+  isActive?: boolean;
 }
 
-const ExerciseTag = ({ label }: TagProps) => {
+const ExerciseTag = ({
+  label,
+  height = 30,
+  borderRadius = 15,
+  isActive = false,
+}: TagProps) => {
   return (
-    <TouchableOpacity className='w-28 h-7 bg-level1 rounded-2xl border border-primary'>
+    <TouchableOpacity
+      className='justify-center border items-center'
+      style={{
+        height,
+        borderRadius,
+        paddingHorizontal: 15,
+        backgroundColor: isActive ? '#1A2836' : '#1B1F27',
+        borderColor: isActive ? '#0E89E5' : '#2B303B',
+      }}
+    >
       <Text
-        className="
-w-20 h-3.5 text-center justify-center text-Textos-em-destaque text-sm font-medium font-['Inter'] leading-5"
+        className='text-center text-sm leading-5'
+        style={{
+          color: isActive ? '#FFFFFF' : '#66758A',
+          fontFamily: 'Inter_500Medium',
+        }}
       >
         {label}
       </Text>
