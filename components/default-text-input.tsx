e5d8168c7f33932d1dc5832be433493680820544
ErickMascarenhas <159568716+ErickMascarenhas@users.noreply.github.com>
2026-05-06 10:49:21 -0300

    feat: add default text input

---
+++ components/default-text-input.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { TextInput, TextInputProps } from 'react-native';
+
+export interface DefaultTextInputProps extends TextInputProps {
+  className?: string;
+}
+
+export function DefaultTextInput({ className, ...rest }: DefaultTextInputProps) {
+  return (
+    <TextInput
+      placeholderTextColor="#465460"
+      className={`bg-level1 px-3.5 py-3 rounded-[10px] outline outline-1 outline-offset-[-1px] outline-outline text-white text-default-2 placeholder:text-placeholder ${className ?? ''}`}
+      {...rest}
+    />
+  );
+}
