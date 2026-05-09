4ed8e09182284ecaeed1d9f9cfbf955537b80960
ErickMascarenhas <159568716+ErickMascarenhas@users.noreply.github.com>
2026-05-09 00:32:01 -0300

    index.tsx rollback

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,6 @@
 import { LoginScreen } from "@/features/auth/screens/login-screen";
-
+import React from "react";
 
 export default function Index() {
-  return LoginScreen;
+  return <LoginScreen />;
 }
