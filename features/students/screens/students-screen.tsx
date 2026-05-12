e76766ef67f2b57c11d51db3e937e724ed91c4d2
Walter Soares Costa Neto <contato.walter@proton.me>
2026-05-12 09:45:42 -0300

    feat: add search-input to students screen

--- features/students/screens/students-screen.tsx
+++ features/students/screens/students-screen.tsx
@@ -1,5 +1,6 @@
 import { Footer } from "@/components/footer";
 import { Header } from "@/components/header";
+import { SearchInput } from "@/components/search-input";
 import React from "react";
 import { View } from "react-native";
 
@@ -8,6 +9,7 @@ export function StudentsScreen() {
     <View className="flex-1 bg-level1">
       <Header />
       <View className="flex-1">
+        <SearchInput containerClassName="mx-4 mt-4" />
       </View>
       <Footer />
     </View>
