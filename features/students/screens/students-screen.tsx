204113193ad2576a54436752ec5dae4c000ac6f5
ErickMascarenhas <159568716+ErickMascarenhas@users.noreply.github.com>
2026-05-12 23:05:56 -0300

    feat(ui): add page header on students screen

--- features/students/screens/students-screen.tsx
+++ features/students/screens/students-screen.tsx
@@ -3,13 +3,22 @@ import { Header } from "@/components/header";
 import { SearchInput } from "@/components/search-input";
 import React from "react";
 import { View } from "react-native";
+import { PageHeader } from "@/components/page-header";
 
 export function StudentsScreen() {
   return (
     <View className="flex-1 bg-level1">
       <Header />
       <View className="flex-1">
-        <SearchInput containerClassName="mx-4 mt-4" />
+        <View className="mx-8 mt-5">
+          <PageHeader
+            mode="inicio"
+            title="Início"
+            subtitle="Selecione um aluno para iniciar uma sessão"
+            onNewPress={() => {}}
+          />
+        </View>
+        <SearchInput containerClassName="mx-8 mt-5" />
       </View>
       <Footer />
     </View>
