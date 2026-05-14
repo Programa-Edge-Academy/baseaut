8d7712cff5a82e83e4ce3c90bd6c15bfd7353fe9
pxdroAndre <andreneto0406@gmail.com>
2026-05-14 10:53:51 -0300

    feat: implementing-new-student-option-on-students-screen

--- features/students/screens/students-screen.tsx
+++ features/students/screens/students-screen.tsx
@@ -3,13 +3,15 @@ import { Header } from "@/components/header";
 import { SearchInput } from "@/components/search-input";
 import React from "react";
 import { View } from "react-native";
+import { StudentCard } from "../components/student-card";
 
 export function StudentsScreen() {
   return (
     <View className="flex-1 bg-level1">
       <Header />
-      <View className="flex-1">
-        <SearchInput containerClassName="mx-4 mt-4" />
+      <View className="flex-1 items-center">
+        <SearchInput containerClassName="mt-4" />
+        <StudentCard className="mt-4" />
       </View>
       <Footer />
     </View>
