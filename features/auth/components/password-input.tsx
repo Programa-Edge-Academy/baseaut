2a9ab7899160d928497794915cba0f131b54c805
ErickMascarenhas <159568716+ErickMascarenhas@users.noreply.github.com>
2026-05-16 23:35:03 -0300

    fix: password input size and border

--- features/auth/components/password-input.tsx
+++ features/auth/components/password-input.tsx
@@ -7,7 +7,6 @@ import {
   View,
   ViewStyle,
 } from "react-native";
-// Importando o ícone (Exemplo com Lucide, substitua pelo seu SVG se preferir)
 import { Eye, EyeOff } from "lucide-react-native";
 import { colors } from "@/assets/colors";
 
@@ -15,15 +14,16 @@ type PasswordInputProps = TextInputProps & {
   className?: string;
   containerStyle?: ViewStyle;
   inputStyle?: TextStyle;
+  outLineBorderClass?: string;
 };
 
 export function PasswordInput({
   className,
   containerStyle,
   inputStyle,
+  outLineBorderClass = "border-outline",
   ...rest
 }: PasswordInputProps) {
-  // Estado para controlar a visibilidade da senha
   const [isPasswordVisible, setIsPasswordVisible] = useState(false);
 
   const togglePasswordVisibility = () => {
@@ -32,11 +32,11 @@ export function PasswordInput({
 
   return (
     <View
-      className={`relative h-11 w-full flex-row items-center rounded-[15px] border border-outline bg-level1 px-4 ${className ?? ""}`}
+      className={`relative h-11 w-full flex-row items-center border bg-level1 ${className} ${outLineBorderClass}`}
       style={containerStyle}
     >
       <TextInput
-        className={`flex-1 pr-10 text-default-2 leading-5 text-white ${inputStyle ? "" : ""}`}
+        className={`flex-1 pr-10 text-default-2 leading-5 text-white rounded-[15px] py-2.5 px-3.5`}
         placeholderTextColor={colors.placeholder}
         secureTextEntry={!isPasswordVisible}
         style={inputStyle}
@@ -46,7 +46,7 @@ export function PasswordInput({
       <Pressable
         onPress={togglePasswordVisibility}
         className="absolute right-3"
-        style={{ padding: 6 }}
+        style={{ padding: 3.5 }}
       >
         {isPasswordVisible ? (
           <EyeOff size={20} color={colors.placeholder} />
