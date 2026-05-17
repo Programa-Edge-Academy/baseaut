e6ab6f7eed829906590845180e5622d3b69f98c8
ErickMascarenhas <159568716+ErickMascarenhas@users.noreply.github.com>
2026-05-16 23:24:45 -0300

    feat: footer routing

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,92 +1,99 @@
 import { BarChart3, Dumbbell, Users } from "lucide-react-native";
-import React, { useState } from "react";
+import React from "react";
 import { colors } from "@/assets/colors";
 import { SafeAreaView } from 'react-native-safe-area-context';
-import { Pressable, Text, StyleSheet, View } from "react-native";
-
-type FooterTab = "activity" | "start" | "analysis";
+import { Pressable, Text, View } from "react-native";
+import { usePathname, useRouter } from "expo-router";
 
 export function Footer() {
-  const [selectedTab, setSelectedTab] = useState<FooterTab>("start");
+  const router = useRouter();
+  const pathname = usePathname();
+  const isActivityActive = (pathname === "/exercises" || pathname === "/circuits");
+  const isStartActive = pathname === "/students";
+  const isAnalysisActive = (pathname === "/analysis" || pathname === "/reports");
+
+  const handleNavigate = (route: "/exercises" | "/students" | "/analysis") => {
+    if (pathname !== route) {
+      router.replace(route as any);
+    }
+  };
 
   return (
     <SafeAreaView edges={['bottom']} className="w-full bg-level2">
       <View className="w-full items-center justify-center bg-level2 p-4">
         <View className="w-full flex-row">
           <Pressable
-            onPress={() => setSelectedTab("activity")}
+            onPress={() => handleNavigate("/exercises")}
             className="flex-1 items-center justify-center gap-1"
           >
-            <View
-              className={
-                selectedTab === "activity" ? "text-primary" : "text-muted"
-              }
-            >
-              <Dumbbell size={28} color={selectedTab === "activity" ? colors.primary : colors.muted} />
+            <View>
+              <Dumbbell 
+                size={28} 
+                color={isActivityActive ? colors.primary : colors.muted} 
+              />
             </View>
 
             <Text
               className={`text-default-1 ${
-                selectedTab === "activity" ? "text-primary" : "text-muted"
+                isActivityActive ? "text-primary" : "text-muted"
               }`}
             >
               Atividades
             </Text>
 
-            {selectedTab === "activity" && (
+            {isActivityActive && (
               <View className="w-1/2 h-[2px] bg-primary mt-1" />
             )}
           </Pressable>
 
           <Pressable
-            onPress={() => setSelectedTab("start")}
+            onPress={() => handleNavigate("/students")}
             className="flex-1 items-center justify-center gap-1"
           >
-            <View
-              className={
-                selectedTab === "start" ? "text-primary" : "text-muted"
-              }
-            >
-              <Users size={28} color={selectedTab === "start" ? colors.primary : colors.muted} />
+            <View>
+              <Users 
+                size={28} 
+                color={isStartActive ? colors.primary : colors.muted} 
+              />
             </View>
 
             <Text
               className={`text-default-1 ${
-                selectedTab === "start" ? "text-primary" : "text-muted"
+                isStartActive ? "text-primary" : "text-muted"
               }`}
             >
               Início
             </Text>
 
-            {selectedTab === "start" && (
+            {isStartActive && (
               <View className="w-1/2 h-[2px] bg-primary mt-1" />
             )}
           </Pressable>
 
           <Pressable
-            onPress={() => setSelectedTab("analysis")}
+            onPress={() => handleNavigate("/analysis")}
             className="flex-1 items-center justify-center gap-1"
           >
-            <View
-              className={
-                selectedTab === "analysis" ? "text-primary" : "text-muted"
-              }
-            >
-              <BarChart3 size={28} color={selectedTab === "analysis" ? colors.primary : colors.muted} />
+            <View>
+              <BarChart3 
+                size={28} 
+                color={isAnalysisActive ? colors.primary : colors.muted} 
+              />
             </View>
 
             <Text
               className={`text-default-1 ${
-                selectedTab === "analysis" ? "text-primary" : "text-muted"
+                isAnalysisActive ? "text-primary" : "text-muted"
               }`}
             >
               Análises
             </Text>
 
-            {selectedTab === "analysis" && (
+            {isAnalysisActive && (
               <View className="w-1/2 h-[2px] bg-primary mt-1" />
             )}
           </Pressable>
+          
         </View>
       </View>
     </SafeAreaView>
